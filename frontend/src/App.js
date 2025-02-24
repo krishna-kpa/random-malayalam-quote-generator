543896a1281fd525b2258199bb1862986c763238
Krishna Prasad A <kpa.krishnaprasada@gmail.com>
2025-02-24 21:58:48 +0530

    css added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,10 @@ import './App.css';
 import Quote from './component/Quote';
 
 function App() {
-
-  const containerStyle = {
-    display: 'flex',
-    justifyContent: 'center',
-    height: '100vh', 
-    alignItems: 'center'
-  };
-
   return (
-   <div style={containerStyle}><Quote/></div>
+    <div className="app-container">
+      <Quote />
+    </div>
   );
 }
 
